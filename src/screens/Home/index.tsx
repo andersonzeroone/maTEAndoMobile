ed5818001ab9db1395183116d285c3ba914a900d
anderson <anderson.pablo02@gmail.com>
2021-06-30 14:56:57 -0300

    commit initial

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,11 @@
+import { func } from 'joi';
+import React from 'react';
+import {View,Text} from 'react-native';
+
+export function Home(){
+  return(
+    <View>
+      <Text>oi</Text>
+    </View>
+  )
+}
